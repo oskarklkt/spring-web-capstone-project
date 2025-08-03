9d38a5374cb69f5726032a4420858ae28c2f4b9b
oklekot <oklekot@griddynamics.com>
2025-08-03 12:57:17 +0200

    initial commit

---
+++ src/main/java/org/example/springwebcapstoneproject/SpringWebCapstoneProjectApplication.java
@@ -0,0 +1,13 @@
+package org.example.springwebcapstoneproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebCapstoneProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebCapstoneProjectApplication.class, args);
+    }
+
+}
